7b0704895ab046b35db0604a205c9190af16dcf4
venkateshguntuka <venkateshguntuka@missionrnd.com>
2016-04-02 20:55:31 +0530

    Submit Commit

--- src/ArraySequences.cpp
+++ src/ArraySequences.cpp
@@ -30,8 +30,74 @@ Difficulty : Medium
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+int aProgression(int *arr, int len, int *result, int pos, int time){
+	int cd1, i, count = 0, ap1pos1 = 0, ap1pos2;
+	cd1 = arr[pos + 1] - arr[pos];
+	for (i = 0; i<len; i++)
+	{
+		if (arr[i + 1] - arr[i] == cd1)
+		{
+			count++;
+		}
+		else
+		{
+			if (count>2)
+			{
+				ap1pos2 = i;
+				if (time == 1)
+				{
+					result[0] = ap1pos1;
+					result[1] = ap1pos2;
+					break;
+				}
+				else
+				{
+					result[2] = ap1pos1;
+					result[3] = ap1pos2;
+					break;
+				}
+			}
+			cd1 = arr[i + 1] - arr[i];
+			ap1pos1 = i;
+		}
+	}
+	return ap1pos2;
 
+}
+void gpProgression(int *arr, int len, int *result)
+{
+	int dt, i = 0, count = 0, gppos1 = 0, gppos2;
+	dt = arr[i + 1] / arr[i];
+	for (i = 0; i<len; i++)
+	{
+		if (arr[i + 1] / arr[i] == dt)
+		{
+			count++;
+		}
+		else
+		{
+			if (count>2)
+			{
+				gppos2 = i;
+				result[4] = gppos1;
+				result[5] = gppos2;
+				break;
+
+			}
+			dt = arr[i + 1] / arr[i];
+			gppos1 = i;
+		}
+	}
+
+}
 int * find_sequences(int *arr, int len){
 	//Return final array which has 6indexes [AP1_S,AP1_E,AP2_S,AP2_E,GP1_S,GP2_E]
-	return NULL;
+	if (len < 0 || arr == NULL)
+		return NULL;
+	int i, result[6] = { 0, 0, 0, 0, 0, 0 };
+	int pos2, pos3;
+	pos2 = aProgression(arr, len, result, 0, 1);
+	pos3 = aProgression(arr, len, result, pos2, 2);
+	gpProgression(arr, len, result);
+	return result;
 }
